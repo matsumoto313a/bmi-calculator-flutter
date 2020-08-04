9f5b8fbf9280e09f3e3c2850d2ed2fa90b653991
matsumoto313a <57129586+matsumoto313a@users.noreply.github.com>
2020-08-04 11:58:31 +0800

    commit - 003

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Center(
-        child: Text('Body Text1'),
+        child: Text('Body Text2'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
