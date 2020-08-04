da0fee8085090c803e9c48a218ffd7bb5528a751
matsumoto313a <57129586+matsumoto313a@users.noreply.github.com>
2020-08-04 16:25:24 +0800

    Extract Widget about Container to ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,13 +19,7 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1F33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(),
                 ),
                 Expanded(
                   child: Container(
@@ -73,6 +67,23 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1F33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
