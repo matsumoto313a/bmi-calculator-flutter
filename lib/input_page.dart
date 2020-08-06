ea19f0b1f91cc5b6675fd2fcdfcaca78dbbb00a0
matsumoto313a <57129586+matsumoto313a@users.noreply.github.com>
2020-08-06 18:58:48 +0800

    Section 138: Functions_as_First_Order_Objects.en
    reusableCard の外に gesture detectorがある。
    今後、reusableCard を別のところで利用する場合、
    それぞれにおいて、あらためてgesture detectorを付記する必要がある。
    下のように。
    
    child: GestureDetector(
      onTap: () {
        setState(() {
          selectedGender = Gender.male;
        });
      },
      child: ReusableCard(
    
    これを変更したい。
    ReusableCard をアップグレードして、タップも検知できるように、
    ReusableCard の中に実装する。
    
    関数もオブジェクト
    データ型は Function
    
    関数も他のデータ型 intやString と同じように変数に代入することができる。
    
    void main() {
      int result = calculator(5, 8, multiply);
    }
    
    int calculator(int n1, int n2, Function calculation) {
      return calculation(n1, n2);
    }
    
    int add(int n1, int n2) {
      return n1 + n2;
    }
    
    int multiply(int n1, int n2) {
      return n1 * n2;
    }
    
    -----
    こんな書き方をすることもできる。
    
    int multiply(int n1, int n2) {
      return n1 * n2;
    }
    
     ↓↓
    
    Function multiply = (int n1, int n2) {
      return n1 * n2;
    };
    
    最後のセミコロンに注意。
    multiply という変数に、関数（無名関数）を代入している。
    この無名関数は、multiply という変数に保存されている。
    
    上の構造は以下。
    
    Function hogehoge = () {};
    
    ---
    クラス内
    
    class Car {
      Ca({this.drive});
      Funtion drive;
    }
    
    void slowDrive() {
      print('diving slowly..');
    }
    
    void fastDrive() {
      print('diving so fast!!');
    }
    
    void main() {
      Car myCar = Car(drive: fastDrive);
      // fastDrive にカッコをつけないように！
      // fastDrive を変数名として使用しているので。
    
      print(myCar.drive());
    
      myCar.drive = slowDrive;
      // slowDrive にカッコをつけないように！
      // slowDrive を変数名として使用しているので。
    }
    
    --- 以下、本番コード--
    まず、GestureDetector を削除。
    （Art Enter -> remove this Widget）
    
      child: GestureDetector(
        onTap: () {
          setState(() {
            selectedGender = Gender.female;
          });
        },
    
    --
    下記により、ReusableCard にonTap時に実行される関数を、
    引数として渡すことができるようになった。
    
     - GestureDetector でwrap
     - Function onTapFuncプロパティを新規追加
     - onTap: プロパティに onTapFuncを渡せるようにした。
    
    class ReusableCard extends StatelessWidget {
    
      final Function onTapFunc;
      ReusableCard({@required this.colour, this.cardChild, this.onTapFunc});
      ---
      return GestureDetector(
        onTap: onTapFunc,

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -34,38 +34,35 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    // onPressに無名関数を渡す
+                    onTapFunc: () {
                       setState(() {
                         selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == Gender.male
-                          ? activeCardColour
-                          : inactiveCardColour,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.mars,
-                        label: 'MALE',
-                      ),
+                    colour: selectedGender == Gender.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
                     ),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onTapFunc: () {
                       setState(() {
                         selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == Gender.female
-                          ? activeCardColour
-                          : inactiveCardColour,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.venus,
-                        label: 'FEMALE',
-                      ),
+                    colour: selectedGender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
                     ),
                   ),
                 ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,20 +1,23 @@
-
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget cardChild;
+  final Function onTapFunc;
 
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.onTapFunc});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onTapFunc,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
